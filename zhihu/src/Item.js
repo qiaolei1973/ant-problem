bb9a821ab7ca18a28694a15b9fffdd6ca452f358
qiaolei <qiaolei1973@gmail.com>
2018-10-26 21:52:38 +0800

    根据属性读值

--- zhihu/src/Item.js
+++ zhihu/src/Item.js
@@ -4,7 +4,8 @@ class App extends Component {
   render() {
     return (
       <div className="list-item">
-        <div >hello world</div>
+        <div >{this.props.title}</div>
+        <div >{this.props.text}</div>
       </div>
     );
   }
